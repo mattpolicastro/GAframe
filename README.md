4040602a795223d0211880d86aaf2d43902ddb7a
Matt Policastro <matt@mattpolicastro.com>
2015-12-24 19:10:48 -0500

    Birth of a repo

---
+++ README.md
@@ -0,0 +1,13 @@
+# GA-Frame
+
+Quick and dirty demo for displaying Google Analytics Realtime API results in A-Frame.
+
+# Next Steps
+
+* Move everything to proper npm modules.
+* Reorganise source.
+* Refactor entity creation:
+	* Use React
+	* Wait for Aframe templates to be rebuilt
+* Integrate [A-Frame text components](https://github.com/ngokevin/aframe-text-component).
+* Make it prettier.
---
+++ vr.js
@@ -0,0 +1,28 @@
+/*global gapi*/
+import CLIENT_ID from './config.js';
+
+gapi.analytics.ready(function() {
+
+  // Step 3: Authorize the user.
+
+	gapi.analytics.auth.authorize({
+		container: 'ga-auth',
+		clientid: CLIENT_ID
+	});
+
+	var realTime = new gapi.analytics.ext.RealTime();
+	var viewSelector = new gapi.analytics.ViewSelector({
+		container: 'ga-view-selector'
+	});
+
+  // Step 6: Hook up the components to work together.
+
+	gapi.analytics.auth.on('success', function() {
+		viewSelector.execute();
+	});
+
+	viewSelector.on('change', function(data) {
+		realTime.set({ids: data, baseline: null}).execute();
+	});
+
+});
